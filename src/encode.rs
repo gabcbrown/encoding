a7e408edc1947604a9899063612ddbe0a01cc72a
Gabriela Brown <gabcbrown@gmail.com>
2021-04-28 14:55:33 -0700

    encoding search works

--- src/encode.rs
+++ src/encode.rs
@@ -43,10 +43,6 @@ fn find_next_match(
         "in find_next_match, starting_position: {:?}",
         starting_position
     );
-    let offset: Option<usize> = None; // negative offset to start of match
-    let length: Option<usize> = None; // length of match
-    let character: Option<u8> = None; // character after match
-                                      // let mut matched: Option<_> = None; // TODO: doesn't need to be mut, should only assign once
 
     // TODO: need utf-e encoding support? if slice hits middle of multicharacter encoding we'll have a problem
     let buffer_window = min(starting_position, search_buffer_size);
@@ -54,36 +50,42 @@ fn find_next_match(
 
     // +1 because .. upper bound is exclusive
     for j in (1..min(buffer_window, lookahead_window) + 1).rev() {
-        println!("j: {:?}", j);
-        println!(
-            "search buffer: {:?}",
-            &input[starting_position - buffer_window..starting_position]
-        );
-        println!(
-            "lookahead buffer: {:?}",
-            &input[starting_position..starting_position + j]
-        );
-        if let Some(matched) = find(
+        // println!("j: {:?}", j);
+
+        if let Some(matched) = search_from_end(
             &input[starting_position - buffer_window..starting_position],
             &input[starting_position..starting_position + j],
         ) {
-            return Some((matched, j, input[starting_position].clone()));
+            println!("matched: {:?}", matched);
+            println!(
+                "search buffer: {:?}",
+                &input[starting_position - buffer_window..starting_position]
+            );
+            println!(
+                "lookahead buffer: {:?}",
+                &input[starting_position..starting_position + j]
+            );
+            println!("buffer_windown - matched: {:?}", buffer_window - matched);
+            return Some((buffer_window - matched, j, input[starting_position].clone()));
         }
     }
+
+    // if we get here, there was no match
+    Some((0, 0, input[starting_position].clone()))
 
     // match matched {
     //     Some(m) => Some(m),
     //     None => Some((0, 0, &input[starting_position].clone())),
     // }
 
-    character.and_then(|c| length.and_then(|l| offset.and_then(|o| Some((o, l, c)))))
+    // character.and_then(|c| length.and_then(|l| offset.and_then(|o| Some((o, l, c)))))
 }
 
-fn find(input1: &[u8], input2: &[u8]) -> Option<usize> {
+fn search_from_end(input1: &[u8], input2: &[u8]) -> Option<usize> {
     if input1.len() < input2.len() {
         None
     } else {
-        for i in 0..input1.len() - input2.len() + 1 {
+        for i in (0..input1.len() - input2.len() + 1).rev() {
             if input1[i..i + input2.len()] == input2[..] {
                 return Some(i);
             }
