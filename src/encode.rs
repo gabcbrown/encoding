bbe48a7870aaa4684342aee2d933a0910f223984
Gabriela Brown <gabcbrown@gmail.com>
2021-04-28 13:00:09 -0700

    wip compiles

--- src/encode.rs
+++ src/encode.rs
@@ -6,14 +6,17 @@ pub enum EncodingError {
 #[derive(Debug)]
 pub struct Encoded(String);
 
-pub fn encode(input: String, lookback_limit: usize) -> Result<Encoded, EncodingError> {
+pub fn encode(
+    input: &str,
+    search_buffer_size: usize,
+    lookahead_buffer_size: usize,
+) -> Result<Encoded, EncodingError> {
     let input_len = input.len();
     let mut starting_position: usize = 0;
-    let search_buffer: Vec<char> = Vec::new();
-    let encoded = Vec::new();
+    let mut encoded = Vec::new();
 
     while starting_position < input_len {
-        let next_match = find_next_match(input, search_buffer, lookback_limit).unwrap();
+        let next_match = find_next_match(input, search_buffer_size, lookahead_buffer_size).unwrap();
         starting_position += next_match.1 + 1;
         encoded.push(next_match);
     }
@@ -22,9 +25,9 @@ pub fn encode(input: String, lookback_limit: usize) -> Result<Encoded, EncodingE
 }
 
 fn find_next_match(
-    input: String,
-    search_buffer: Vec<char>,
-    lookback_limit: usize,
+    input: &str,
+    search_buffer_size: usize,
+    lookahead_buffer_size: usize,
 ) -> Option<(usize, usize, char)> {
     let offset: Option<usize> = None; // negative offset to start of match
     let length: Option<usize> = None; // length of match
--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,21 @@ struct Opts {
     /// Input to be encoded
     input: String,
     #[clap(short, long, default_value = "6")]
-    lookback_limit: usize,
+    search_buffer_size: usize,
+    #[clap(short, long, default_value = "4")]
+    lookahead_buffer_size: usize,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
 
     println!("Encoding input string: {:?}", opts.input);
-    let encoded = encode(opts.input, opts.lookback_limit).unwrap();
+    let encoded = encode(
+        &opts.input,
+        opts.search_buffer_size,
+        opts.lookahead_buffer_size,
+    )
+    .unwrap();
     println!("Encoded: {:?}", encoded);
     println!("Decoding");
     let decoded = decode(encoded).unwrap();
