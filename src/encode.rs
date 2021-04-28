467aeeac939c0a735da2b360a99a431a74fa735d
Gabriela Brown <gabcbrown@gmail.com>
2021-04-28 12:51:49 -0700

    wip

--- src/encode.rs
+++ src/encode.rs
@@ -6,6 +6,28 @@ pub enum EncodingError {
 #[derive(Debug)]
 pub struct Encoded(String);
 
-pub fn encode(input: String) -> Result<Encoded, EncodingError> {
+pub fn encode(input: String, lookback_limit: usize) -> Result<Encoded, EncodingError> {
+    let input_len = input.len();
+    let mut starting_position: usize = 0;
+    let search_buffer: Vec<char> = Vec::new();
+    let encoded = Vec::new();
+
+    while starting_position < input_len {
+        let next_match = find_next_match(input, search_buffer, lookback_limit).unwrap();
+        starting_position += next_match.1 + 1;
+        encoded.push(next_match);
+    }
+
     Err(EncodingError::Unimplemented)
+}
+
+fn find_next_match(
+    input: String,
+    search_buffer: Vec<char>,
+    lookback_limit: usize,
+) -> Option<(usize, usize, char)> {
+    let offset: Option<usize> = None; // negative offset to start of match
+    let length: Option<usize> = None; // length of match
+    let character: Option<char> = None; // character after match
+    character.and_then(|c| length.and_then(|l| offset.and_then(|o| Some((o, l, c)))))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,22 @@ mod encode;
 use decode::decode;
 use encode::encode;
 
-use clap::{AppSettings, Clap};
+use clap::Clap;
 
 #[derive(Clap)]
 #[clap(version = "0.1.0")]
 struct Opts {
     /// Input to be encoded
     input: String,
+    #[clap(short, long, default_value = "6")]
+    lookback_limit: usize,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
 
     println!("Encoding input string: {:?}", opts.input);
-    let encoded = encode(opts.input).unwrap();
+    let encoded = encode(opts.input, opts.lookback_limit).unwrap();
     println!("Encoded: {:?}", encoded);
     println!("Decoding");
     let decoded = decode(encoded).unwrap();
