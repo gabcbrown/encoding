d2e21d3e46474996981c327fb0702aa4fc2aefdb
Gabriela Brown <gabcbrown@gmail.com>
2021-04-28 12:07:03 -0700

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(version = "0.1.0")]
+struct Opts {
+    /// Input to be encoded
+    input: String,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Test(Test),
+}
+
+/// Tool for running encoding/decoding test suite
+#[derive(Clap)]
+#[clap(version = "0.1.0")]
+struct Test {
+    /// Print debug info
+    #[clap(short)]
+    debug: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    println!("Using input file: {}", opts.input);
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Test(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
+
+    // more program logic goes here...
 }
