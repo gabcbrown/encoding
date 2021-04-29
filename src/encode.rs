d35c3628d1d7284246e9f88a53a104cc4104ad46
Gabriela Brown <gabcbrown@gmail.com>
2021-04-28 17:40:27 -0700

    add canterbury corpus

--- src/encode.rs
+++ src/encode.rs
@@ -15,19 +15,19 @@ pub fn encode(
 ) -> Result<Encoded, EncodingError> {
     let bytes = input.as_bytes();
     let input_len = bytes.len();
-    let mut starting_position: usize = 0;
+    let mut starting_index: usize = 0;
     let mut encoded = Vec::new();
 
     // TODO: verify what happens in edge case starting pos = input len... etc.
-    while starting_position < input_len {
+    while starting_index < input_len {
         let next_match = find_next_match(
             bytes,
-            starting_position,
+            starting_index,
             search_buffer_size,
             lookahead_buffer_size,
         )
         .ok_or(EncodingError::UnexpectedEndOfString)?; // TODO: correct error?
-        starting_position += next_match.1 + 1;
+        starting_index += next_match.1 + 1;
         encoded.push(next_match);
     }
 
@@ -37,29 +37,29 @@ pub fn encode(
 // TODO: need utf-e encoding support? if slice hits middle of multicharacter encoding we'll have a problem
 fn find_next_match(
     input: &[u8],
-    starting_position: usize,
+    starting_index: usize,
     search_buffer_size: usize,
     lookahead_buffer_size: usize,
 ) -> Option<(usize, usize, u8)> {
-    let buffer_window = min(starting_position, search_buffer_size);
-    let lookahead_window = min(input.len() - starting_position, lookahead_buffer_size);
+    let buffer_window = min(starting_index, search_buffer_size);
+    let lookahead_window = min(input.len() - starting_index - 1, lookahead_buffer_size); // - 1 because length and index are are off by 1 from each other, and we need to know the indexes available
 
     // +1 because .. upper bound is exclusive
     for j in (1..min(buffer_window, lookahead_window) + 1).rev() {
         if let Some(matched) = search_from_end(
-            &input[starting_position - buffer_window..starting_position],
-            &input[starting_position..starting_position + j],
+            &input[starting_index - buffer_window..starting_index],
+            &input[starting_index..starting_index + j],
         ) {
             return Some((
                 buffer_window - matched,
                 j,
-                input[starting_position + j].clone(),
+                input[starting_index + j].clone(),
             ));
         }
     }
 
     // if we get here, there was no match
-    Some((0, 0, input[starting_position].clone()))
+    Some((0, 0, input[starting_index].clone()))
 }
 
 fn search_from_end(input1: &[u8], input2: &[u8]) -> Option<usize> {
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,15 +3,65 @@
 
 use manta::{decode::decode, encode::encode};
 
-// Basic tests
+fn assert_roundtrip(input: &str) {
+    let encoded = encode(input, 6, 4);
+    assert!(encoded.is_ok());
+
+    let decoded = decode(encoded.unwrap());
+    assert!(decoded.is_ok());
+
+    assert_eq!(input, decoded.unwrap());
+}
+
+// Correctness tests
 #[datatest::files("tests/data/ascii", {
     input in r"^(.*)\.txt",
 })]
 #[test]
-fn ascii(input: &str) {
-    let encoded = encode(input, 6, 4);
-    assert!(encoded.is_ok());
-    let decoded = decode(encoded.unwrap());
-    assert!(decoded.is_ok());
-    assert_eq!(input, decoded.unwrap());
+fn small_ascii(input: &str) {
+    assert_roundtrip(input)
+}
+
+#[datatest::files("tests/data/unicode", {
+    input in r"^(.*)\.txt",
+})]
+#[test]
+fn small_unicode(input: &str) {
+    assert_roundtrip(input)
+}
+
+// Research Corpus
+// from: https://corpus.canterbury.ac.nz/descriptions/
+// Some files were excluded because they failed to parse as valid UTF-8, a requirement for the datatest framework, unfortunately.
+
+#[datatest::files("tests/data/cantrbry", {
+    input in r"^(.*)",
+})]
+#[test]
+fn cantrbry(input: &str) {
+    assert_roundtrip(input)
+}
+
+#[datatest::files("tests/data/artificl", {
+    input in r"^(.*)",
+})]
+#[test]
+fn artificl(input: &str) {
+    assert_roundtrip(input)
+}
+
+#[datatest::files("tests/data/large", {
+    input in r"^(.*)",
+})]
+#[test]
+fn large(input: &str) {
+    assert_roundtrip(input)
+}
+
+#[datatest::files("tests/data/misc", {
+    input in r"^(.*)",
+})]
+#[test]
+fn misc(input: &str) {
+    assert_roundtrip(input)
 }
